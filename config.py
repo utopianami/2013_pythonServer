9cb3145201889750b3fa9970b8f1aa9b82e2d826
sunghwan <sunghwanjo@nhnnext.org>
2013-10-21 19:11:18 +0900

    add contents in config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
+
+ADMINS = frozenset(['namul10@gmail.com'])
+SECRET_KEY = 'VERYVERYSECRET'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
+DATABASE_CONNEXT_OPTIONS = {}
+
+
