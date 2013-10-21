c72df35e322a41024dfc6c85be5e21a88817b8f4
sunghwan <sunghwanjo@nhnnext.org>
2013-10-21 19:10:32 +0900

    add User Model in models.py

---
+++ app/users/models.py
@@ -0,0 +1,14 @@
+from aap import db
+
+class User(db.Model):
+	__tablename__ = 'users_user'
+	id = db.Column(db.Integer, primary_key = True)
+	email = db.Column(db.String(120), unique=True)
+	password = db.Column(db.String(120))
+
+	def __init__(self, email=None, password=None)
+		self.password = password;
+		self.email = email
+
+	def __repr__(self):
+		return '<User %r>' % (self.name)
